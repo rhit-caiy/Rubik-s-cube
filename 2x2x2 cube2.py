c9bcd50d488f68c665acac2f34786a6aab4a703a
rhit-caiy <caiy@rose-hulman.edu>
2022-11-10 22:42:33 -0500

    add htm and qtm

--- 2x2x2 cube2.py
+++ 2x2x2 cube2.py
@@ -1,4 +1,4 @@
-import time,sys
+import time,sys,threading
 from itertools import permutations,product
 from math import log
 
@@ -16,7 +16,6 @@ for i in permutations(range(7)):
     cdict[i]=n
     n+=1
 print(len(cdict))
-
 cpddict={}
 n=0
 for i in product(*cornerdirection):
@@ -30,41 +29,79 @@ def rotatecubeaddress(f,t,c,cd):
     cn1,cn2,cn3,cn4=c[c1],c[c2],c[c3],c[c4]=c[nc1],c[nc2],c[nc3],c[nc4]
     ftd=facetimedirection[f][t]
     cd[cn1],cd[cn2],cd[cn3],cd[cn4]=ftd[cd[cn1]],ftd[cd[cn2]],ftd[cd[cn3]],ftd[cd[cn4]]
+
+def gethtm():
+    global htm
+    cubelist=[[cblock,cblockd,1,-1]]
+    newcubelist=[]
+    key=cdict[tuple(cblock)]*2187+cpddict[tuple(cblockd)]
+    htm[key]=1
+    tstart=time.time()
+    for step in range(1,17):
+        t1=time.time()
+        for cube in cubelist:
+            c,cd,oldstep,f1=cube
+            for f in range(3):
+                if f==f1:
+                    continue
+                for t in range(1,4):
+                    nc=c.copy()
+                    ncd=cd.copy()
+                    rotatecubeaddress(f,t,nc,ncd)
+                    key=cdict[tuple(nc)]*2187+cpddict[tuple([ncd[i] for i in nc])]
+                    newstep=oldstep*9+f*3+t-1
+                    if key not in htm:
+                        htm[key]=newstep
+                        newcubelist.append([nc,ncd,newstep,f])
+        print("{:<10}{:<20}{:<20}{:f}".format(step,len(htm),"",time.time()-t1))
+        if newcubelist==[]:
+            break
+        cubelist=newcubelist
+        newcubelist=[]
+    print("htm",time.time()-tstart)
+
+def getqtm():
+    global qtm
+    cubelist=[[cblock,cblockd,1]]
+    newcubelist=[]
+    key=cdict[tuple(cblock)]*2187+cpddict[tuple(cblockd)]
+    qtm[key]=1
+    tstart=time.time()
+    for step in range(1,17):
+        t1=time.time()
+        for cube in cubelist:
+            c,cd,oldstep=cube
+            for f in range(3):
+                for t in (1,3):
+                    nc=c.copy()
+                    ncd=cd.copy()
+                    rotatecubeaddress(f,t,nc,ncd)
+                    key=cdict[tuple(nc)]*2187+cpddict[tuple([ncd[i] for i in nc])]
+                    newstep=oldstep*9+f*3+t-1
+                    if key not in qtm:
+                        qtm[key]=newstep
+                        newcubelist.append([nc,ncd,newstep])
+        print("{:<10}{:<20}{:<20}{:f}".format(step,"",len(qtm),time.time()-t1))
+        if newcubelist==[]:
+            break
+        cubelist=newcubelist
+        newcubelist=[]
+    print("qtm",time.time()-tstart)
+    
+print("{:<10}{:<20}{:<20}{}".format("step","htm","qtm","time"))
+print("{:<10}{:<20}{:<20}{:f}".format(0,1,1,0))
 htm={}
 qtm={}
-cubelist=[[cblock.copy(),cblockd.copy(),1,-1,True]]
-newcubelist=[]
-key=cdict[tuple(cblock)]*2187+cpddict[tuple(cblockd)]
-htm[key]=1
-qtm[key]=1
-tstart=time.time()
-print("{:<6}{:<12}{:<12}{}".format("step","htm","qtm","time"))
-print("{:<6}{:<12}{:<12}{:f}".format(0,1,1,0))
-for step in range(1,17):
-    t1=time.time()
-    for cube in cubelist:
-        c,cd,step,f1,isqtm=cube
-        for f in range(3):
-            for t in range(1,4):
-                nc=c.copy()
-                ncd=cd.copy()
-                rotatecubeaddress(f,t,nc,ncd)
-                key=cdict[tuple(nc)]*2187+cpddict[tuple([ncd[i] for i in nc])]
-                newstep=step*9+f*3+t-1
-                isqtm2=isqtm and t!=2 and key not in qtm
-                if key not in htm:
-                    htm[key]=newstep
-                    newcubelist.append([nc,ncd,newstep,f,isqtm2])
-                elif key not in qtm and isqtm and t!=2:
-                    newcubelist.append([nc,ncd,newstep,f,isqtm2])
-                if isqtm2:
-                    qtm[key]=newstep
-    print("{:<6}{:<12}{:<12}{:f}".format(step,len(htm),len(qtm),time.time()-t1))
-    if newcubelist==[]:
-        break
-    cubelist=newcubelist
-    newcubelist=[]
-print(time.time()-tstart,"s")
+dict1thread=threading.Thread(target=gethtm)
+dict2thread=threading.Thread(target=getqtm)
+tdictstart=time.time()
+dict1thread.start()
+dict2thread.start()
+dict2thread.join()
+dict1thread.join()
+tdictend=time.time()
+print("{:<10}{:<20}{:<20}{:f}".format("total",len(htm),len(qtm),tdictend-tdictstart))
+
 def printdictsize(d):
     a=sys.getsizeof(d)
     b=0
@@ -73,6 +110,8 @@ def printdictsize(d):
     for i in d.values():
         b+=sys.getsizeof(i)
     print([name for name in globals() if globals()[name] is d][0],len(d),"dict space",a,"B    values and keys space",b,"B    total",(a+b)/1000000000,"GB    ",(a+b)/1000000,"MB")
+printdictsize(cdict)
+printdictsize(cpddict)
 printdictsize(htm)
 printdictsize(qtm)
 s=0
